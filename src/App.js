10325dac9d612108cf0c4ee2c6e98b6ab66d2f3d
Divyam <divyamsaxena123@gmail.com>
2020-08-02 18:12:45 +0530

    Added Styles to cartItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import CartItem from './Cartitem';
 
 function App() {
   return (
     <div className="App">
-      <h1>CART</h1>
+           <CartItem /> 
     </div>
   );
-}
+} 
+
+
 
 export default App;
