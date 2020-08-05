cab790e05217c44cdfd3a5309721220b5786528b
Divyam <divyamsaxena123@gmail.com>
2020-08-05 19:24:25 +0530

    Added props

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    constructor (){
-        super();
-        this.state ={
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img : ''
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
+    
     increaseQuantity = () => {
         
         // setState form 1
@@ -29,7 +20,7 @@ class CartItem extends React.Component {
 
     decreaseQuantity = () => {
         const {qty} = this.state;
-        if(qty == 0){
+        if(qty === 0){
             return;
         }
         // setState form 2
@@ -41,11 +32,11 @@ class CartItem extends React.Component {
     }
 
     render(){
-        const {price , title , qty} = this.state;
+        const {price , title , qty} = this.props.product;
         return(
             <div className='cart-item'>
                 <div className="left-block">
-                    <img  style={styles.image}/>
+                    <img  style={styles.image} alt=""/>
                  </div>
                 <div className="right-block">
                     <div style={{fontSize: 25}}>{title}</div>
