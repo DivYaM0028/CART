18b244e883f86a52db755c6c1c5fea3b8fa4a6db
Divyam <divyamsaxena123@gmail.com>
2020-08-29 22:00:17 +0530

    Added Firebase

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,57 +1,46 @@
 import React from 'react';
-// Added funcitonal component
-const CartItem = (props) => {
-    const {price , title , qty} = props.product;
-    const {
-        product,
-        onIncreaseQuantity,
-        onDecreaseQuantity,
-        onDeleteProduct
-    } = props;
-    return(
-        <div className='cart-item'>
-            <div className="left-block">
-                <img  style={styles.image} src={product.img} alt=""/>
-                </div>
-            <div className="right-block">
-                <div style={{fontSize: 25}}>{title}</div>
-                <div style={{fontSize: 25}}>Rs: {price} </div>
-                <div style={{fontSize: 25}}>Qty: {qty}</div>
-                <div className="cart-item-actions">
-                    <img 
-                        alt="increase" 
-                        className="action-icons"
-                        src="https://image.flaticon.com/icons/svg/992/992651.svg" 
-                        onClick={() => props.onIncreaseQuantity(props.product)}
-                        />
-                    <img
-                        alt="decrease" 
-                        className="action-icons" 
-                        src="https://image.flaticon.com/icons/svg/992/992683.svg" 
-                        onClick={() => props.onDecreaseQuantity(props.product)} 
-                                                    
-                    />
-                    <img 
-                        alt="delete"
-                        className="action-icons"
-                        src="https://image.flaticon.com/icons/svg/1214/1214428.svg" 
-                        onClick={() => props.onDeleteProduct(product.id)}
-                    />
-                </div>
-            </div>
 
+class CartItem extends React.Component {
+  render () {
+    const { qty, price, title } = this.props.product;
+    const { product } = this.props;
+
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img
+            style={{ height: 110, width: 110, borderRadius: 5, background: '#ccc' }}
+            src={product.img}
+          />
         </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}> {title} </div>
+          <div style={{ color: '#777' }}>{price}</div>
+          <div style={{ color: '#777' }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            <img 
+              alt="add"
+              src="https://image.flaticon.com/icons/svg/1665/1665578.svg"
+              className="action-icons"
+              onClick={() => this.props.onIncreaseQuantity(product)}
+            />
+            <img
+              alt="minus"
+              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              className="action-icons"
+              onClick={() => this.props.onDecreaseQuantity(product)}
+            />
+            <img
+              alt="delete"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+              className="action-icons"
+              onClick={() => this.props.onDeleteProduct(product.id)}
+            />
+          </div>
+        </div>
+      </div>
     )
-}
-
-const styles = {
-    image:{
-      height: 150,
-      width: 150,
-      borderRadius: 4,
-      background: '#ccc'
-    }
   }
-  
+}
 
 export default CartItem;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCPwUULemeaHnzhMaNTIjLZmaRhFo5uiRg",
+  authDomain: "cart-aa8ff.firebaseapp.com",
+  databaseURL: "https://cart-aa8ff.firebaseio.com",
+  projectId: "cart-aa8ff",
+  storageBucket: "cart-aa8ff.appspot.com",
+  messagingSenderId: "315684662655",
+  appId: "1:315684662655:web:3ccf0e73626f0498ce7e4a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
